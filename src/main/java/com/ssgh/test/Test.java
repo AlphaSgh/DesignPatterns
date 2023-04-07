4e8ba33ad070c97b267cb4fae06019ae74e7a108
AlphaSgh <upupsgh@163.com>
2023-04-07 17:15:11 +0800

    DesignPatterns 20230407

--- src/main/java/com/ssgh/test/Test.java
+++
@@ -1,9 +0,0 @@
-package com.ssgh.test;
-public class Test {
-    public static void main(String args[]) {
-        int[] a = new int[10];
-        float[] b = new float[10];
-        System.out.println(a.getClass());
-        System.out.println(b.getClass());
-    }
-}
